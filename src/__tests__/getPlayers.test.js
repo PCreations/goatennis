f2237891d48f72942a5d04ee3618958f9878d814
Pierre Criulanscy <pcriulan@gmail.com>
2019-11-29 00:46:24 +0100

    /players now retrieves data and sends the result sorted by id asc

--- src/__tests__/getPlayers.test.js
+++ src/__tests__/getPlayers.test.js
@@ -1,10 +1,43 @@
 const request = require("supertest");
-const { app } = require("../server");
+const { makeApp } = require("../server");
 
 describe("GET /players", () => {
   it("should return a 200 http response with correct header", async () => {
+    const app = makeApp();
     const response = await request(app).get("/players");
     const contentTypeHeader = response.get("Content-Type");
     expect(contentTypeHeader).toContain("application/json");
+  });
+  test("given a list of data it should return the list of data ordered by id", async () => {
+    const data = [
+      {
+        id: 3,
+        foo: "bar"
+      },
+      {
+        id: 1,
+        foo: "baz"
+      },
+      {
+        id: 2,
+        foo: "foobaz"
+      }
+    ];
+    const app = makeApp({ getDataAsync: () => Promise.resolve(data) });
+    const response = await request(app).get("/players");
+    expect(response.body).toEqual([
+      {
+        id: 1,
+        foo: "baz"
+      },
+      {
+        id: 2,
+        foo: "foobaz"
+      },
+      {
+        id: 3,
+        foo: "bar"
+      }
+    ]);
   });
 });
--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 
-const app = express();
+const byIdAsc = ({ id: idA }, { id: idB }) =>
+  parseInt(idA, 10) - parseInt(idB, 10);
 
-app.use("/players", (_, res) => {
-  res.status(200).json({});
-});
+const makeApp = ({ getDataAsync = () => Promise.resolve([]) } = {}) => {
+  const app = express();
+
+  app.use("/players", async (_, res) => {
+    const data = await getDataAsync();
+    data.sort(byIdAsc);
+    res.status(200).json(data);
+  });
+
+  return app;
+};
 
 module.exports = {
-  app
+  makeApp
 };
