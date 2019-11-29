460d8d8fa37b373450859d95819f06a447de360b
Pierre Criulanscy <pcriulan@gmail.com>
2019-11-29 01:07:03 +0100

    add 200 test for GET /players/<id>

--- src/__tests__/getPlayer.test.js
+++ src/__tests__/getPlayer.test.js
@@ -5,9 +5,18 @@ const playersFixture = require("./fixture.json");
 describe("GET /players/<id>", () => {
   it("given some players should return a 404 error if no player found", async () => {
     const app = makeApp({
-      getDataAsync: Promise.resolve(playersFixture.players)
+      getDataAsync: () => Promise.resolve(playersFixture.players)
     });
     const response = await request(app).get("/players/0");
     expect(response.statusCode).toEqual(404);
+  });
+  it("given some players should return a 200 code with the correct player if player exists", async () => {
+    const { players } = playersFixture;
+    const app = makeApp({
+      getDataAsync: () => Promise.resolve(players)
+    });
+    const response = await request(app).get(`/players/${players[0].id}`);
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(players[0]);
   });
 });
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const byIdAsc = ({ id: idA }, { id: idB }) =>
   parseInt(idA, 10) - parseInt(idB, 10);
 
+const byId = playerId => player =>
+  parseInt(player.id, 10) === parseInt(playerId, 10);
+
 const makeApp = ({ getDataAsync = () => Promise.resolve([]) } = {}) => {
   const app = express();
 
@@ -12,7 +15,13 @@ const makeApp = ({ getDataAsync = () => Promise.resolve([]) } = {}) => {
     res.status(200).json(data);
   });
 
-  app.get("/players/:playerId", async (_, res) => {
+  app.get("/players/:playerId", async (req, res) => {
+    const data = await getDataAsync();
+    const { playerId } = req.params;
+    const player = data.find(byId(playerId));
+    if (player) {
+      res.status(200).json(player);
+    }
     res.status(404).send();
   });
 
